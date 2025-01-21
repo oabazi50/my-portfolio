b49aa6d49be60a0a3919ac2ccf3b79d85376ee67
oabazi50 <oabazi50@gmail.com>
2025-01-21 20:38:16 +0100

    Add files via upload

---
+++ myportofolio.js
@@ -0,0 +1,16 @@
+
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+       
+        if (this.getAttribute('href').startsWith('#')) {
+            e.preventDefault(); 
+
+            
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+        
+    });
+});
